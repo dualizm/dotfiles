af73a8c0c3dfaadedcdca1e3ad2bd9cdaea6f913
notidman <laiker35@yandex.ru>
2022-09-16 08:07:48 +0400

    [upd]

--- conky/check_ethernet.sh
+++ conky/check_ethernet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 netname=$(nmcli | grep -w "connected to" | awk '{print $NF}');
-netip=$(nmcli | grep -w "connected to" -A 4 | grep -w "inet4" | awk '{print $NF}')
+# netip=$(nmcli | grep -w "connected to" -A 4 | grep -w "inet4" | awk '{print $NF}')
 
 if [[ `nmcli | grep -w "connected"` == *"connected"* ]]
 then
   # printf " $netip $netname";
-  printf " $netname";
+  printf "";
 else
   printf "";
 fi
